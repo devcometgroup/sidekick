fff662cc912cc47779d26dd48d9e60118f7b621c
gyulanemeth <gyula.nemeth85@gmail.com>
2013-03-07 19:02:38 +0100

    Datagrid: function scope bug fixed

--- src/Sidekick-Bootstrap/Sidekick-Bootstrap-Datagrid.js
+++ src/Sidekick-Bootstrap/Sidekick-Bootstrap-Datagrid.js
@@ -1,37 +1,28 @@
-define(["jquery","ko"], function($, ko) {
-	var content;
-	var header;
-	
-	
+define(["jquery", "ko"], function($, ko) {
 	var update = function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext){
 		var values = valueAccessor();
+		
 		//var options = ko.utils.unwrapObservable(values.options);
 		var dataSource = ko.utils.unwrapObservable(values.dataSource);
 		
 		var pagination= ko.utils.unwrapObservable(values.pagination);
 		
-		var elem=$(element);
+		var elem = $(element);
 		
 		elem.empty();
 		
+		var header = $(element).find('thead')[0].outerHTML;
+		var content = $(element).find('tbody')[0].innerHTML;
 		
 		
+		//GY: kesobb ez igy nem lesz jo - async requestek miatt... at kene adni neki egy callback-et, amit meghivva
+		//beallithatjuk az items ertekeit...
+		//Az itemsnel biztositani kene, hogy observableArray legyen, mert kulonben a foreach nem updateli a view-t miutan
+		//az ajax request utan beallitottuk a callback-kel az ertekeket...
 		var items = dataSource.read(pagination.elementPerPage,ko.utils.unwrapObservable(pagination.actualPage));
 		
 		elem.append(header);
-		var display = content; 
-		/*
-		var tbody = $("<tbody />");
-		
-		for (var i = 0; i<items.length; i+=1){
-			row= $("<tr />");
-			for (var j = 0; j<display.length; j=j+1){
-				$("<td />").append(items[i][display[j]]).appendTo(row);
-				
-			}
-			row.appendTo(tbody);
-		}
-		elem.append(tbody);*/
+
 		var tbody = $("<tbody data-bind=\"foreach: items\">");
 		tbody.append(content);
 		
@@ -49,12 +40,9 @@ define(["jquery","ko"], function($, ko) {
 
 	ko.bindingHandlers.datagrid = {
 			init:function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext){
-			
-				header = $(element).find('thead')[0].outerHTML;
-
-				content = $(element).find('tbody')[0].innerHTML;
-							
-				return { 'controlsDescendantBindings': true };
+				return {
+					controlsDescendantBindings: true
+				};
 			},
 			update: update
 	};
