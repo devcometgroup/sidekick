7c1d308f532b8c420e29ed16a9f56e76c25f2cfc
gyulanemeth <gyula.nemeth85@gmail.com>
2013-03-09 22:21:54 +0100

    coding style...

--- src/Sidekick-Bootstrap/Sidekick-Bootstrap-OrderController.js
+++ src/Sidekick-Bootstrap/Sidekick-Bootstrap-OrderController.js
@@ -1,30 +1,28 @@
 define(["jquery", "ko"], function($, ko) {
 	ko.bindingHandlers.orderController = {
-			init:function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext){
-				console.log(valueAccessor());
-			},
-			update:function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext){
-				var values = valueAccessor();
-				var field = values.field;
-				var ascDesc = "none";
-				elem=$(element);
-				elem.click(function(){
-					if (ascDesc==="none"){
-						ascDesc="asc";
-					} else if (ascDesc==="asc"){
-						ascDesc="desc";
-					} else if (ascDesc=="desc"){
-						ascDesc="none";
-					}
-					if (values.callback!==undefined){
-						values.callback.callback(field,ascDesc);
-					} else if(bindingContext.callback!==undefined) {
-						bindingContext.callback(field,ascDesc);
-							
-					}
-					
-					});
-			}
-	
+		init:function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+			//
+		},
+		update:function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+			var values = valueAccessor();
+			var field = values.field;
+			var ascDesc = "none";
+			elem = $(element);
+			elem.click(function() {
+				if (ascDesc === "none") {
+					ascDesc = "asc";
+				} else if (ascDesc === "asc") {
+					ascDesc = "desc";
+				} else if (ascDesc === "desc") {
+					ascDesc = "none";
+				}
+				
+				if (typeof values.callback !== "undefined") {
+					values.callback.callback(field,ascDesc);
+				} else if (typeof bindingContext.callback !== undefined) {
+					bindingContext.callback(field,ascDesc);
+				}
+			});
+		}
 	};
 });
