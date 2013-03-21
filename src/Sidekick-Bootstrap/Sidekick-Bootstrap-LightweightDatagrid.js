2568e2576ed4dd0afb11dda55ae76cd012b6fd58
Reizinger Krisztián <reizinger.krisztian@gmail.com>
2013-03-21 18:24:32 +0100

    lightweigth datagrid insert delete

--- src/Sidekick-Bootstrap/Sidekick-Bootstrap-LightweightDatagrid.js
+++ src/Sidekick-Bootstrap/Sidekick-Bootstrap-LightweightDatagrid.js
@@ -17,7 +17,7 @@ define(["jquery", "ko"], function($, ko) {
 			if(values.options.pagination.selectorValue){
 				ko.computed(function (){
 					if (values.options.pagination.selectorValue()==="all"){
-						values.options.pagination.elementPerPage(values.options.pagination.elementNumber);
+						values.options.pagination.elementPerPage(ko.utils.unwrapObservable(values.options.pagination.elementNumber));
 					} else {
 						values.options.pagination.elementPerPage(ko.utils.unwrapObservable(values.options.pagination.selectorValue));
 					}
@@ -46,6 +46,10 @@ define(["jquery", "ko"], function($, ko) {
 			var header =  ko.utils.unwrapObservable(values.options.header);
 			var updateButton = ko.utils.unwrapObservable(values.options.updateButton);
 			
+			var deleteButton = ko.utils.unwrapObservable(values.options.deleteButton);
+			var insertButton = ko.utils.unwrapObservable(values.options.insertButton);
+			
+			
 			var elem = $(element);
 
 			elem.empty();
@@ -56,7 +60,7 @@ define(["jquery", "ko"], function($, ko) {
 				orderingKeys = Object.keys(values.options.orderings);
 				for(var i = 0; i<orderingKeys.length; i+=1){
 					if (values.options.orderings[orderingKeys[i]].field !== orderBy){
-						values.options.orderings[orderingKeys[i]].ascDesc( "none" );
+						values.options.orderings[orderingKeys[i]].ascDesc= "none" ;
 					}			
 				}
 				
@@ -67,94 +71,256 @@ define(["jquery", "ko"], function($, ko) {
 			
 
 			
-			var table = $("<table class=\"table table-bordered\" />");
+			var table = $("<table class=\"table table-bordered\" style=\"table-layout:fixed; word-wrap:break-word; \"/>");
 			
+			var thead = $("<thead />");
+			
+			var createHeader = function(actThead){
+				actThead.empty();
+				for (var i = 0; i<header.length; i+=1){
+					if(header[i].orderable || header[i].orderable ==="" ){
+						
+						actHeader = header[i];
+						
+						//var th = $("<th data-bind=\"orderController: options.orderings.by"+header[i].prop+"\"></th>");
+						if (!values.options.orderings["by"+actHeader.prop]){
+							values.options.orderings["by"+actHeader.prop]={field:actHeader.prop,ascDesc:"none",label:actHeader.label};						
+						}
+						var th = $("<th />");
+						
+						
+						var label = values.options.orderings["by"+actHeader.prop].label;
+						var ascDesc = values.options.orderings["by"+actHeader.prop].ascDesc;
+						var field = values.options.orderings["by"+actHeader.prop].field;
+						
+						var link = $("<button class=\"btn btn-link\">").append(label);
+	
+						if (ascDesc === "asc") {
+							link = link.append($("<i class=\" icon-chevron-up\">"));
+						} else if (ascDesc === "desc") {
+							link = link.append($("<i class=\" icon-chevron-down\">"));
+						} else if (ascDesc === "none") {
+							link = link.append($("<i class=\" icon-minus\">"));
+						}
+						
+						link.click (function(actElement) {
+							return function(){
+								if (actElement.ascDesc === "none") {
+									actElement.ascDesc="asc";
+								} else if (actElement.ascDesc === "asc") {
+									actElement.ascDesc="desc";
+								} else if (actElement.ascDesc === "desc") {
+									actElement.ascDesc ="none";
+								}
+								
+								values.callback(actElement.field,actElement.ascDesc);
+							
+							};
+						}(values.options.orderings["by"+actHeader.prop]));
+						link.appendTo(th);
+						
+					} else {
+						var th = $("<th>"+header[i].label+"</th>");
+					}
+					th.appendTo(actThead);
+				}
+				
+				if(updateButton || deleteButton || insertButton){
+					actThead.append($("<th/>"));
+				}
+			};
+			
+			createHeader(thead);
+			
+			var createNormalCell = function(item,actHeader,cell){
+				if (actHeader.type!=="bool"){
+					cell.append( item[actHeader.prop]);						
+				}
+				else {
+					if (item[actHeader.prop]){
+						cell.append("<input type=\"checkbox\" checked=\"checked\" disabled=\"true\">");
+					} else {
+						cell.append("<input type=\"checkbox\" disabled=\"true\">");
+					}
+				}
+			};
+			
+			var createEditableCell = function(actHeader,cell,item){
+				
+				if (actHeader.type==="number"){
+					var input = $("<input class=\"input-block-level\" type=number name=\""+actHeader.prop+"\"/>");
+					if (item!==null && item!==undefined && item[actHeader.prop]!== undefined){
+						input[0].value=item[actHeader.prop];
+					}
+					
+				} else if (actHeader.type==="text"){
+					var input =$("<input class=\"input-block-level\" type=text name=\""+actHeader.prop+"\" />");
+
+					if (item!==null && item!==undefined && item[actHeader.prop]!== undefined){
+						input[0].value=item[actHeader.prop];
+					}
+				} else if (actHeader.type==="bool"){
+					if (item!==null && item!==undefined && item[actHeader.prop]){
+						var input = $("<input type=\"checkbox\" checked=\"checked\" name=\""+actHeader.prop+"\"  >");
+					} else {
+						var input = $("<input type=\"checkbox\" name=\""+actHeader.prop+"\"  >");
+					}
+				}
+				
+				cell.append(input);
+			};
 			
 			
 			var createRow = function(item,actTr){
 				for (var j = 0; j<header.length; j+=1){
-					var td = $("<td/>").append( item[header[j].prop]);
+					var td= $("<td/>");
+					createNormalCell(item,header[j],td);
 					td.appendTo(actTr);
 				}
-				if (updateButton){
-					var updateTemp= $("<td/>");
-					var buttonTemp=$("<button>");
-					if (updateButton.icon){
-						if (updateButton.label){
-							buttonTemp.append($("<i class=\""+updateButton.icon+"\"></i>")).append(updateButton.label);
-						} else {
-							buttonTemp.append($("<i class=\""+updateButton.icon+"\"/>"));
-						}
-					} else if (updateButton.label){
-						buttonTemp.append(updateButton.label);
-					} else {
-						buttonTemp.append($("<i class=\"icon-edit\"/>"));
-					}
+				if (updateButton || deleteButton || insertButton){
+					var buttonsTemp= $("<td/>");
 					
 					
-					var callback  = function(item,actTr){
+					if (updateButton){
 						
-						return function(){
-							
-							/*for(var i = 0; i<orderingKeys.length; i+=1){
-								values.options.orderings[orderingKeys[i]].ascDesc( "none" );
-										
-							}*/
-							
-							actTr.empty();
-							for (var j = 0; j<header.length; j+=1){
-								if (header[j].type==="number"){
-									var input = $("<input class=\"input-block-level\" type=number name=\""+header[j].prop+"\"/>");
-									input[0].value=item[header[j].prop];
-									
-									//.append( items()[i][header[j].prop]);
-								} else if (header[j].type==="text"){
-									var input =$("<input class=\"input-block-level\" type=text name=\""+header[j].prop+"\" />");
-									//.append( items()[i][header[j].prop]);
-									input[0].value=item[header[j].prop];
-								}
-								var td = $("<td/>").append(input);
-								td.appendTo(actTr);
-
-								
-								
-								
+						var updateTemp=$("<button>");
+						if (updateButton.icon){
+							if (updateButton.label){
+								updateTemp.append($("<i class=\""+updateButton.icon+"\"></i>")).append(updateButton.label);
+							} else {
+								updateTemp.append($("<i class=\""+updateButton.icon+"\"/>"));
 							}
+						} else if (updateButton.label){
+							updateTemp.append(updateButton.label);
+						} else {
+							updateTemp.append($("<i class=\"icon-edit\"/>"));
+						}
+						
+						
+						var callback  = function(item,actTr){
 							
-							var td=$("<td/>");
-							var ok = $("<button />").append($("<i class=\"icon-ok\">"));
-							var updateAct = function(item,actTr){
-								return function(){		
-									for (var j = 0; j<header.length; j+=1){
-										item[header[j].prop] = actTr.find("input[name=\""+header[j].prop+"\"]")[0].value;
+							return function(){
+								actTr.empty();
+								for (var j = 0; j<header.length; j+=1){
+									if (header[j].editable!==false){
+										
+									var td = $("<td/>");
+									createEditableCell(header[j],td,item);
+									
+									
+									} else {
+										var td = $("<td/>");
+										createNormalCell(item,header[j],td);
 									}
 									
-							dataSource.update(item);
-							actTr.empty();
-							createRow(item,actTr);
-								};
-							};
-							ok.click(updateAct(item,actTr));
-							ok.appendTo(td);
-							
-							var cncl =  $("<button/>").append($("<i class=\"icon-remove\">"));
-							var cancel = function(){
-								return function(){
+									
+									
+									td.appendTo(actTr);
+	
+									
+									
+									
+								}
+								
+								var td=$("<td/>");
+								var ok = $("<button />").append($("<i class=\"icon-ok\">"));
+								var updateAct = function(item,actTr){
+									return function(){
+									for(var i = 0; i<orderingKeys.length; i+=1){
+										if (values.options.orderings[orderingKeys[i]].ascDesc !== "none"){
+											values.options.orderings[orderingKeys[i]].ascDesc = "none";
+											createHeader(thead);
+										} 
+									}
+									for (var j = 0; j<header.length; j+=1){
+										if (header[j].editable!==false){
+											
+											if (header[j].type!=="bool"){
+												item[header[j].prop] = actTr.find("input[name=\""+header[j].prop+"\"]")[0].value;										
+											} else {
+												item[header[j].prop] = $(actTr.find("input[name=\""+header[j].prop+"\"]")[0]).prop('checked');
+											}
+										}
+									}
+											
+									dataSource.update(item);
 									actTr.empty();
 									createRow(item,actTr);
+									};
 								};
+								ok.click(updateAct(item,actTr));
+								ok.appendTo(td);
+								
+								var cncl =  $("<button/>").append($("<i class=\"icon-remove\">"));
+								var cancel = function(actTr){
+									return function(){
+										actTr.empty();
+										createRow(item,actTr);
+									};
+								};
+								cncl.click(cancel(actTr));
+								cncl.appendTo(td);
+								
+								td.appendTo(actTr);
 							};
-							cncl.click(cancel());
-							cncl.appendTo(td);
+						};
+						
+						updateTemp.click(callback(item,actTr));
+						updateTemp.appendTo(buttonsTemp);
+					}
+				
+				if(deleteButton){
+					var deleteTemp=$("<button>");
+					if (deleteButton.icon){
+						if (deleteButton.label){
+							deleteTemp.append($("<i class=\""+deleteButton.icon+"\"></i>")).append(deleteButton.label);
+						} else {
+							deleteTemp.append($("<i class=\""+deleteButton.icon+"\"/>"));
+						}
+					} else if (deleteButton.label){
+						deleteTemp.append(deleteButton.label);
+					} else {
+						deleteTemp.append($("<i class=\"icon-trash\"/>"));
+					}
+					
+					var del = function(actItem){
+						return function(){
 							
-							td.appendTo(actTr);
+						buttonsTemp.empty();
+						buttonsTemp.append("<p>"+deleteButton.question+"</p>");
+						var ok = $($("<button />").append($("<i class=\"icon-ok\">")).append(deleteButton.yes));
+						var delcall = function(item,actTr){
+							return function(){
+								dataSource.del(actItem);
+								//actTr.empty();
+								dataSource.read(ko.utils.unwrapObservable(pagination.elementPerPage),ko.utils.unwrapObservable(pagination.actualPage),callbackRead,orderBy,ascDesc);
+								
+							};
+						};
+						ok.click(delcall(item,actTr));
+						ok.appendTo(buttonsTemp);
+						
+						var cncl =  $($("<button/>").append($("<i class=\"icon-remove\">")).append(deleteButton.cancel));
+						var delcancel = function(actTr){
+							return function(){
+								actTr.empty();
+								createRow(item,actTr);
+							};
+						};
+						cncl.click(delcancel(actTr));
+						cncl.appendTo(buttonsTemp);
+						
+						//td.appendTo(actTr);
+						
 						};
 					};
 					
-					buttonTemp.click(callback(item,actTr));
-					updateTemp.append(buttonTemp);
-					updateTemp.appendTo(actTr);
+					deleteTemp.click(del(item));
+					deleteTemp.appendTo(buttonsTemp);
 					
+				}
+				
+				buttonsTemp.appendTo(actTr);
 				}
 				
 			};
@@ -190,29 +356,111 @@ define(["jquery", "ko"], function($, ko) {
 				}			
 			}
 			
-			var thead = $("<thead />");
 			
 			
-			for (var i = 0; i<header.length; i+=1){
-				if(header[i].orderable){
-					var th = $("<th data-bind=\"orderController: options.orderings.by"+header[i].prop+"\"></th>");
-					if (!values.options.orderings["by"+header[i].prop]){
-						values.options.orderings["by"+header[i].prop]={field:header[i].prop,ascDesc:ko.observable("none"),label:header[i].label};						
-					}
-				} else {
-					var th = $("<th>"+header[i].label+"</th>");
-				}
-				th.appendTo(thead);
-			}
 			
-			if(updateButton){
-				thead.append($("<th/>"));
-			}
 			
 			thead.appendTo(table);
 			
 
 			dataSource.read(ko.utils.unwrapObservable(pagination.elementPerPage),ko.utils.unwrapObservable(pagination.actualPage),callbackRead,orderBy,ascDesc);
+			
+			
+			if (insertButton){
+				tfoot = $("<tfoot/>");
+				var createFooter = function (actTr){
+					actTr.empty();
+					for (var j = 0; j<header.length; j+=1){
+						var td = $("<td/>");
+						td.appendTo(tfoot);
+					}
+					var buttonsTemp= $("<td/>");
+				
+					
+					var insertTemp=$("<button>");
+					if (insertButton.icon){
+						if (insertButton.label){
+							insertTemp.append($("<i class=\""+insertButton.icon+"\"></i>")).append(insertButton.label);
+						} else {
+							insertTemp.append($("<i class=\""+insertButton.icon+"\"/>"));
+						}
+					} else if (insertButton.label){
+						insertTemp.append(insertButton.label);
+					} else {
+						insertTemp.append($("<i class=\"icon-edit\"/>"));
+					}
+					
+					var insertcallback = function (actTr){
+						return function(){
+	
+							actTr.empty();
+							for (var j = 0; j<header.length; j+=1){
+						
+								if (header[j].insertable!==false){
+									
+									var td = $("<td/>");
+									createEditableCell(header[j],td);
+								} else {
+									var td = $("<td/>");
+								}
+								td.appendTo(tfoot);
+		
+							}
+							
+		
+							var td=$("<td/>");
+							var ok = $("<button />").append($("<i class=\"icon-ok\">"));
+							var insertNew = function(actTr){
+								return function(){
+									var item={};
+									
+									for (var j = 0; j<header.length; j+=1){
+										if (header[j].insertable!==false){
+												
+											if (header[j].type!=="bool"){
+												item[header[j].prop] = actTr.find("input[name=\""+header[j].prop+"\"]")[0].value;										
+											} else {
+												item[header[j].prop] = $(actTr.find("input[name=\""+header[j].prop+"\"]")[0]).prop('checked');
+											}
+										}
+									}
+									
+									
+									
+								dataSource.insert(item);
+								};
+							};
+							ok.click(insertNew(actTr));
+							ok.appendTo(td);
+							
+							var cncl =  $("<button/>").append($("<i class=\"icon-remove\">"));
+							var cancel = function(actTr){
+								return function(){
+									createFooter(actTr);
+								};
+							};
+							cncl.click(cancel(actTr));
+							cncl.appendTo(td);
+							
+							td.appendTo(actTr);
+						};
+					};
+					
+					insertTemp.click(insertcallback(actTr));
+					
+					insertTemp.appendTo(buttonsTemp);
+					
+					buttonsTemp.appendTo(actTr);
+				};
+				
+			createFooter(tfoot);
+				
+			tfoot.appendTo(table);
+			}
+				
+				
+			
+			
 			
 			
 
